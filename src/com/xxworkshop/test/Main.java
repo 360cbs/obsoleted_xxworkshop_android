aff9b7736460da27b4608fd3e34a0ed85a698530
Broche Xu <xym0519@gmail.com>
2014-02-12 17:04:03 +0800

    init

---
+++ src/com/xxworkshop/test/Main.java
@@ -0,0 +1,10 @@
+package com.xxworkshop.test;
+
+/**
+ * Created by brochexu on 2/12/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
